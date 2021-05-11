bf09e38bf2242203383407fdf7e9b7a41aa9a291
TheWiseDwayne <83955118+TheWiseDwayne@users.noreply.github.com>
2021-05-11 10:16:34 +0200

    Update Sudoku.py

--- Sudoku.py
+++ Sudoku.py
@@ -18,7 +18,7 @@ class Sudoku:
     def getPosition(self,row,column):
         return self.array[row][column]
     
-     #Method given a row a column and a value returns True if all the comprovations are True and change the selected value
+     #Method that given a row a column and a value returns True if all the comprovations are True and change the selected value
     def changeValue(self,row,column,value):
         if self.comZero(row,column)==True:
             if self.comRow(row,value):
@@ -28,37 +28,44 @@ class Sudoku:
                         return True
         return False
         
-    #Method given a column and a column returns True if there isn't a zero in the selected position
+    #Method that given a column and a column returns True if there isn't a zero in the selected position
     def comZero(self,row,column):
         if self.array[row][column]==0:return True
         return False
         
-    #Method given a row and a value returns True if there aren't any repeated value in the row     
+    #Method that given a row and a value returns True if there aren't any repeated value in the row     
     def comRow(self,row,value):
         for x in range(len(self.array)):
             if self.array[row][x] == value: return False      
         return True
     
-    #Method given a column and a value returns True if there aren't any repeated value in the column 
+    #Method that given a column and a value returns True if there aren't any repeated value in the column 
     def comColumn(self,column,value):
         for x in range(len(self.array)):
             if self.array[x][column] == value: return False 
         return True
     
-    #Method given a row a column and a value returns True if there aren't any repeated value in the square
+    #Method that given a row a column and a value returns True if there aren't any repeated value in the square
     def comSquare(self,row,column,value):
         row //= 3
         row *= 3
         column //= 3
         column *= 3
         square =[]
-        y=row
-        x=column
-        for w in range(y,row+3):
-            for h in range(x,column+3):
+
+        for w in range(row,row+3):
+            for h in range(column,column+3):
                 square.extend(str(self.array[w][h]))
         
         if str(value) in str(square):
             return False
         else:
             return True      
+    
+    #Method that checks if theres in any 0, if there isn't any returns True which means that there is a winner 
+    def gameFinished(self):
+        for x in range(len(self.array)):
+            for y in range(len(self.array[0])):
+                if 0 in self.array:
+                    return False
+        return True
